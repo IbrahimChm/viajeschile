b181c956dd9cd2e03dc2411a8fc51c284057fe97
IbrahimCh <ibrahimchm@gmail.com>
2023-04-10 21:19:09 -0400

    añadido formulario de contacto y más texto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-	$('p').text('Hola, soy un párrafo modificado con jQuery!');
-});
+    $('#carouselExampleIndicators').carousel({
+      interval: 2000
+    });
+  });
+  $(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top
+          }, 1000);
+      }
+    });
+  });
+  function scrollToSection(id) {
+    $('html, body').animate({
+      scrollTop: $(id).offset().top
+    }, 1000);
+  }
+    
