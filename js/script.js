0af52204ff840dc83865316a163f7fc6e75483fb
IbrahimCh <ibrahimchm@gmail.com>
2023-04-10 22:34:47 -0400

    añadido footer

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 5000,
+  wrap: true
+})
 $(document).ready(function(){
-    $('#carouselExampleIndicators').carousel({
-      interval: 2000
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        var $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
     });
   });
-  $(document).ready(function() {
-    $('a[href^="#"]').on('click', function(event) {
-      var target = $(this.getAttribute('href'));
-      if( target.length ) {
-          event.preventDefault();
-          $('html, body').stop().animate({
-              scrollTop: target.offset().top
-          }, 1000);
-      }
-    });
-  });
-  function scrollToSection(id) {
-    $('html, body').animate({
-      scrollTop: $(id).offset().top
-    }, 1000);
-  }
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
     
